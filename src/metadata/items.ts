59d9eaf2ce57a4eedd0f7ff65a5f72a3c53653f8
Elias Chew <101262042+elias7905@users.noreply.github.com>
2023-09-04 14:35:16 +0800

    Update Boost Descriptions of Boost Wearables (#17)

--- src/metadata/items.ts
+++ src/metadata/items.ts
@@ -1,7 +1,7 @@
 import { BumpkinItem, ITEM_IDS } from "features/bumpkins/types/Items";
 import { DETAILS } from "features/bumpkins/types/ItemDetails";
 
-type DisplayType = "boost_percentage" | "number";
+type DisplayType = "boost_percentage" | "boost_number";
 
 export type Attribute = {
   display_type?: DisplayType;
@@ -41,6 +41,141 @@ export const EXTRA_DETAILS: Partial<Record<BumpkinItem, Details>> = {
         display_type: "boost_percentage",
         trait_type: "Increased Cake SFL at Grub Shop",
         value: 20,
+      },
+    ],
+  },
+  "Fruit Picker Apron": {
+    boosts: [
+      {
+        display_type: "boost_number",
+        trait_type: "Increase Fruit Yield",
+        value: 0.1,
+      },
+    ],
+  },
+  "Angel Wings": {
+    boosts: [
+      {
+        display_type: "boost_percentage",
+        trait_type: "Chance of Instant Crops",
+        value: 30,
+      },
+    ],
+  },
+  "Devil Wings": {
+    boosts: [
+      {
+        display_type: "boost_percentage",
+        trait_type: "Chance of Instant Crops",
+        value: 30,
+      },
+    ],
+  },
+  "Eggplant Onesie": {
+    boosts: [
+      {
+        display_type: "boost_number",
+        trait_type: "Increase Eggplant Yield",
+        value: 0.1,
+      },
+    ],
+  },
+  "Golden Spatula": {
+    boosts: [
+      {
+        display_type: "boost_percentage",
+        trait_type: "Increase EXP gains",
+        value: 10,
+      },
+    ],
+  },
+  "Mushroom Hat": {
+    boosts: [
+      {
+        display_type: "boost_number",
+        trait_type: "Increase Mushroom Yield",
+        value: 0.1,
+      },
+    ],
+  },
+  Parsnip: {
+    boosts: [
+      {
+        display_type: "boost_percentage",
+        trait_type: "Increase Parsnip Yield",
+        value: 20,
+      },
+    ],
+  },
+  "Sunflower Amulet": {
+    boosts: [
+      {
+        display_type: "boost_percentage",
+        trait_type: "Increase Sunflower Yield",
+        value: 10,
+      },
+    ],
+  },
+  "Carrot Amulet": {
+    boosts: [
+      {
+        display_type: "boost_percentage",
+        trait_type: "Carrot Growth Time",
+        value: -20,
+      },
+    ],
+  },
+  "Beetroot Amulet": {
+    boosts: [
+      {
+        display_type: "boost_percentage",
+        trait_type: "Increase Beetroot Yield",
+        value: 20,
+      },
+    ],
+  },
+  "Sunflower Shield": {
+    boosts: [
+      {
+        display_type: "boost_number",
+        trait_type: "Cost of Sunflower Seeds",
+        value: 0,
+      },
+    ],
+  },
+  "Green Amulet": {
+    boosts: [
+      {
+        display_type: "boost_percentage",
+        trait_type: "Crop Critical Hit Yield",
+        value: 900, // 900% more yield=10x
+      },
+    ],
+  },
+  Cattlegrim: {
+    boosts: [
+      {
+        display_type: "boost_number",
+        trait_type: "Increase Animal Yield",
+        value: 0.25,
+      },
+    ],
+  },
+  "Luna's Hat": {
+    boosts: [
+      {
+        display_type: "boost_percentage",
+        trait_type: "Cooking time",
+        value: -50,
+      },
+    ],
+  },
+  "Infernal Pitchfork": {
+    boosts: [
+      {
+        display_type: "boost_number",
+        trait_type: "Increase Crop Yield",
+        value: 3,
       },
     ],
   },
