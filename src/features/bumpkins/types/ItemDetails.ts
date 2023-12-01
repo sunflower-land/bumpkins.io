ac191a3093df25089619fa6e67cd52fac2486a52
spencerdezartsmith <17863697+spencerdezartsmith@users.noreply.github.com>
2023-12-01 12:38:14 +1100

    [FEAT] Ugly Christmas Sweater

--- src/features/bumpkins/types/ItemDetails.ts
+++ src/features/bumpkins/types/ItemDetails.ts
@@ -1181,5 +1181,8 @@ export const DETAILS: Record<BumpkinItem, ItemDetails> = {
     description:
       "Weather the storm in style with this hat featuring the turbulent tentacles of an octopus caught in a tempest.",
     part: "hat",
+  "Ugly Christmas Sweater": {
+    description: "A whimsical holiday wearable from Earn Alliance",
+    part: "shirt",
   },
 };
